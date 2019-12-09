6610433de15185e267d302848adcd3d2bea52fb3
nelson888 <tambue@hotmail.fr>
2019-12-09 07:22:58 +0100

    added test

--- src/test/groovy/com/serversquad/polytech/mailapp/mailapp/MailappApplicationTests.groovy
+++ src/test/groovy/com/serversquad/polytech/mailapp/mailapp/MailappApplicationTests.groovy
@@ -1,13 +1,35 @@
 package com.serversquad.polytech.mailapp.mailapp
 
+import com.serversquad.polytech.mailapp.mailapp.model.mail.StoredEmail
+import com.serversquad.polytech.mailapp.mailapp.repository.email.EmailRepository
+import com.serversquad.polytech.mailapp.mailapp.service.EmailParser
+import com.serversquad.polytech.mailapp.mailapp.service.EmailParserTest
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+
+import static org.junit.Assert.assertNotNull
 
 @SpringBootTest
 class MailappApplicationTests {
+	@Autowired
+	private EmailRepository emailRepository;
+
+	private final EmailParser emailParser = new EmailParser();
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { }
+
+	@Test
+	public void storeEmail() throws Exception {
+		StoredEmail storedEmail = emailParser.parseEmail(EmailParserTest.class.getResourceAsStream("/mail.xml"));
+		emailRepository.saveEmail(storedEmail);
+	}
+
+	@Test
+	public void getEmail() throws Exception {
+		StoredEmail storedEmail = emailRepository.getById(0).get();
+		assertNotNull(storedEmail);
 	}
 
 }
