ce609b2fdf4be8b5b6d57158dca1df212ec64818
nelson888 <tambue@hotmail.fr>
2020-01-13 12:36:36 +0100

    added local configuration

--- src/main/groovy/com/serversquad/polytech/mailapp/mailapp/configuration/LocalConfiguration.groovy
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/configuration/LocalConfiguration.groovy
@@ -1,5 +1,9 @@
 package com.serversquad.polytech.mailapp.mailapp.configuration
 
+import com.serversquad.polytech.mailapp.mailapp.repository.email.EmailRepository
+import com.serversquad.polytech.mailapp.mailapp.repository.email.LocalStorageEmailRepository
+import com.serversquad.polytech.mailapp.mailapp.service.EmailParser
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
@@ -7,5 +11,9 @@ import org.springframework.context.annotation.Profile
 @Configuration
 class LocalConfiguration {
 
+    @Bean
+    EmailRepository emailRepository(EmailParser emailParser) {
+        return new LocalStorageEmailRepository(emailParser)
+    }
 
 }
