7c92a1257db4b8f949ef6163061d8510fa40c014
nelson888 <tambue@hotmail.fr>
2020-01-13 10:23:53 +0100

    created controller

---
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/controller/GroupController.groovy
@@ -0,0 +1,11 @@
+package com.serversquad.polytech.mailapp.mailapp.controller
+
+import io.swagger.annotations.Api
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/groups")
+@Api(value = "Controller to manipulates groups")
+class GroupController {
+}
---
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/controller/ParticipantController.groovy
@@ -0,0 +1,12 @@
+package com.serversquad.polytech.mailapp.mailapp.controller
+
+import io.swagger.annotations.Api
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/participants")
+@Api(value = "Controller to manipulates participants")
+class ParticipantController {
+
+}
