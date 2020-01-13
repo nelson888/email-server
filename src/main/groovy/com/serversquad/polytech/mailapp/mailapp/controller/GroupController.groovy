f402aca7be4110aad4e5694aa7b2673047a382c0
arijsk <arijsakhraoui18@gmail.com>
2020-01-13 11:19:57 +0100

    implemented group controller

--- src/main/groovy/com/serversquad/polytech/mailapp/mailapp/controller/GroupController.groovy
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/controller/GroupController.groovy
@@ -1,6 +1,10 @@
 package com.serversquad.polytech.mailapp.mailapp.controller
 
+import com.serversquad.polytech.mailapp.mailapp.repository.group.GroupRepository
 import io.swagger.annotations.Api
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +12,19 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/groups")
 @Api(value = "Controller to manipulates groups")
 class GroupController {
+
+    private final GroupRepository grouprepository;
+
+    GroupController(GroupRepository grouprepository) {
+        this.grouprepository = grouprepository;
+    }
+    @PostMapping("/{id}")
+    ResponseEntity getById(@PathVariable("id") String id) {
+        return ResponseEntity.of(grouprepository.getById(id))
+    }
+
+    @PostMapping("/all")
+    ResponseEntity getByAll() {
+        return ResponseEntity.ok(grouprepository.getAll())
+    }
 }
