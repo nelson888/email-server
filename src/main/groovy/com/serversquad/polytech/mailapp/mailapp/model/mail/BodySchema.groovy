3079bc90c64961414ad4965d6f49fae5368b451c
nelson888 <tambue@hotmail.fr>
2020-01-16 11:33:06 +0100

    added xml validator

--- src/main/groovy/com/serversquad/polytech/mailapp/mailapp/model/mail/BodySchema.groovy
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/model/mail/BodySchema.groovy
@@ -2,6 +2,7 @@ package com.serversquad.polytech.mailapp.mailapp.model.mail
 
 import com.serversquad.polytech.mailapp.mailapp.excepetion.MalformedXmlException
 import com.serversquad.polytech.mailapp.mailapp.repository.email.body.AbstractBodyRepository
+import com.serversquad.polytech.mailapp.mailapp.service.XmlValidator
 
 import javax.xml.XMLConstants
 import javax.xml.transform.stream.StreamSource
@@ -15,15 +16,6 @@ class BodySchema {
 
 
     void validate(String xml) throws MalformedXmlException {
-        try {
-            SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-                    .newSchema(new URL(url))
-                    .newValidator()
-                    .validate(new StreamSource(new ByteArrayInputStream(
-                            AbstractBodyRepository.xmlContent(xml, name).bytes
-                    )))
-        } catch (Exception e) {
-            throw new MalformedXmlException(e)
-        }
+        XmlValidator.validate(xsd, AbstractBodyRepository.xmlContent(xml, name))
     }
 }
---
+++ src/main/groovy/com/serversquad/polytech/mailapp/mailapp/service/XmlValidator.groovy
@@ -0,0 +1,27 @@
+package com.serversquad.polytech.mailapp.mailapp.service
+
+import com.serversquad.polytech.mailapp.mailapp.excepetion.MalformedXmlException
+
+import javax.xml.XMLConstants
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
+
+class XmlValidator {
+
+    private static StreamSource stringSource(String s) {
+        return new StreamSource(new ByteArrayInputStream(
+                s.bytes
+        ))
+    }
+
+    static void validate(String xsd, String xmlContent) throws MalformedXmlException {
+        try {
+            SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+                    .newSchema(stringSource(xsd))
+                    .newValidator()
+                    .validate(stringSource(xmlContent))
+        } catch (Exception e) {
+            throw new MalformedXmlException(e)
+        }
+    }
+}
